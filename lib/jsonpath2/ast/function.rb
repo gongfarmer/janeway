8431757a6cb8cb7072b31f6ebcba3bd873b1750f
Fraser Hanson <fraser.hanson@gmail.com>
2024-12-30 20:36:34 -0800

    parser: parse functions

---
+++ lib/jsonpath2/ast/function.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module JsonPath2
+  module AST
+    # Represents a JSONPath built-in function.
+    class Function < JsonPath2::AST::Expression
+      alias name value
+
+      attr_reader :parameters, :body
+
+      # FIXME: provide function body too as a proc?
+      def initialize(name, parameters, &body)
+        raise ArgumentError, "expect string, got #{name.inspect}" unless name.is_a?(String)
+
+        super(name)
+        @parameters = parameters
+        @body = body
+        raise "expect body to be a Proc, got #{body.class}" unless body.is_a?(Proc)
+      end
+
+      def to_s
+        "#{name}(#{@parameters.join(',')})"
+      end
+    end
+  end
+end
---
+++ spec/lib/jsonpath2/parser/function_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'jsonpath2'
+
+module JsonPath2
+  describe Parser do
+    describe '#parse_function' do
+      it 'parses the length function' do
+        ast = described_class.parse('$[?length(@.authors) >= 5]')
+        expect(ast).to eq('$[?(length(@.authors) >= 5)]')
+      end
+
+      it 'parses the count function' do
+        ast = described_class.parse('$[?count(@.*.author) >= 5]', LOG)
+        expect(ast).to eq('$[?count(@.*.author) >= 5]')
+      end
+    end
+  end
+end
